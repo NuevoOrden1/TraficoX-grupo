83b1e9ade335114b1a74630a92df03bd1a6b430c
Alulo <alan.uni739@gmail.com>
2025-05-31 02:22:29 -0500

    Inicio del versionado del proyecto con git

--- src/main/java/com/mycompany/traficox/TraficoX.java
+++ src/main/java/com/mycompany/traficox/TraficoX.java
@@ -11,6 +11,6 @@ package com.mycompany.traficox;
 public class TraficoX {
 
     public static void main(String[] args) {
-        System.out.println("Hello Krilin");
+        System.out.println("prueba gitHub");
     }
 }
