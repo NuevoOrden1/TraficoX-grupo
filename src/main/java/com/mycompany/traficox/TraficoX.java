787081f75015c25bb02d8facce4ac218606e1be0
Alulo <alan.uni739@gmail.com>
2025-05-31 01:42:51 -0500

    Inicio del versionado del proyecto con git

---
+++ src/main/java/com/mycompany/traficox/TraficoX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.traficox;
+
+/**
+ *
+ * @author a
+ */
+public class TraficoX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Krilin");
+    }
+}
